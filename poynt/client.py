eaa584692210c965b7651f87ed3dc3558d74e796
Kyle Dugger <kedugger@gmail.com>
2026-08-28 23:47:50 -0700

    trying StartAt as a string

--- poynt/client.py
+++ poynt/client.py
@@ -263,7 +263,7 @@ class PoyntClient:
         params = {
             "limit": limit,
             "start_at": start_at,
-            "startAt": start_at
+            "startAt": str(start_at)
         }
 
         async with httpx.AsyncClient(timeout=30.0) as client:
